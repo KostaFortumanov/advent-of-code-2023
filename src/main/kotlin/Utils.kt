f5e0731f66a73cd292bd8ac8646150e86855f2d5
Kosta Fortumanov <kfortumanov@gmail.com>
2023-12-01 08:54:10 +0100

    Day 1

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,3 +1,7 @@
 import java.io.File
 
 fun readFileLines(day: Int): List<String> = File({}.javaClass.getResource("input_$day.txt")!!.path).readLines()
+
+fun String.digits() = mapNotNull { it.digitToIntOrNull() }
+
+fun Any?.println() = println(this)
--- src/main/kotlin/day01/Day1.kt
+++ src/main/kotlin/day01/Day1.kt
@@ -1,7 +1,42 @@
 package day01
 
+import digits
+import println
 import readFileLines
 
 fun main() {
     val input = readFileLines(1)
+
+    part1(input).println()
+    part2(input).println()
+}
+
+private fun part1(input: List<String>) =
+    input.map { it.digits() }
+        .sumOf { it.first() * 10 + it.last() }
+
+private fun part2(input: List<String>): Int {
+    val digitMap = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9,
+    )
+    return input.map { line ->
+        digitMap.entries.flatMap { (k, v) ->
+            listOf(
+                line.windowed(k.length).indexOfFirst { it == k } to digitMap[k],
+                line.windowed(k.length).indexOfLast { it == k } to digitMap[k],
+                line.indexOfFirst { it == v.digitToChar() } to v,
+                line.indexOfLast { it == v.digitToChar() } to v,
+            )
+        }.filter { (index, _) -> index != -1 }
+            .sortedBy { (index, _) -> index }
+            .mapNotNull { (_, value) -> value }
+    }.sumOf { it.first() * 10 + it.last() }
 }
